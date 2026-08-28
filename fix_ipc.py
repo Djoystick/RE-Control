71594cdcc629616ce106a6a20ea3113acdf45abc
Djoystick <j.j.niccals2@gmail.com>
2026-08-28 12:54:02 +0300

    chore: remove leftover automation scripts from project root

--- fix_ipc.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-with open(r"H:\Work\RE_Control\src\main\index.ts", "r", encoding="utf-8") as f:
-    text = f.read()
-
-text = text.replace("votingManager.handleCommand(`ComboBot_${i}`, optionId.toString());", "votingManager.handleChatCommand({ username: `ComboBot_${i}`, command: optionId.toString(), args: [] });")
-
-with open(r"H:\Work\RE_Control\src\main\index.ts", "w", encoding="utf-8") as f:
-    f.write(text)
-print("Fixed ipc handler")
